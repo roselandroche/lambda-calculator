6774273f1359397fe9c0ab382ba50d4e8b849aea
roselandroche <r.landroche@gmail.com>
2019-08-22 19:11:49 -0400

    Setting project up to work on after lecture

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -2,7 +2,7 @@ import React from "react";
 
 //import any components needed
 
-//Import your array data to from the provided data file
+//Import your array data to from the provided data file 
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
