b070b071ba7c66ffc415a5e5985e1fd70a3d904a
roselandroche <r.landroche@gmail.com>
2019-08-23 19:24:20 -0400

    Im so confused about everything

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
