64b6136b7e74cf261a55fa225411c0fc6d8b0182
roselandroche <r.landroche@gmail.com>
2019-08-22 21:51:09 -0400

    A bit confused at step 3 - name in what provided file?

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -14,6 +14,9 @@ const Numbers = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {numbers.map(item => {
+         <button>`${item}`</button>
+       })}
     </div>
   );
 };
