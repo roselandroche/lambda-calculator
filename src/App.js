51206f89b1e32648b23cbc222450e095949f9dd7
roselandroche <r.landroche@gmail.com>
2019-08-23 20:34:49 -0400

    Why can't there just be clear instructions?

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Specials from "./components/ButtonComponents/SpecialButtons/Specials"
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
-// import NumberButton from "./components/ButtonComponents/NumberButtons/NumberButton";
+import Display from './components/DisplayComponents/Display';
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -21,6 +21,7 @@ function App() {
   return (
     <div className="container">
       <Logo />
+      <Display />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
         <Numbers />
